582541880c61eb0f4d2d1c1f0575a99fa05d6e08
JYJ <dudwns5013@naver.com>
2024-07-08 15:39:30 +0900

    [front][feat] initialize project

---
+++ frontend/src/Router.jsx
@@ -0,0 +1,16 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home/Home';
+import SignUp from './pages/SignUp/SignUp';
+
+const Router = () => {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/user/sign-up' element={<SignUp />} />
+      </Routes>
+    </BrowserRouter>
+  );
+};
+
+export default Router;
